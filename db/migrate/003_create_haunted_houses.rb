8d5dce2ac691fd3886afaf2bb7997f16b3250644
Mitchel Parish <mitchelparish@gmail.com>
2019-02-19 19:04:26 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,6 +1,6 @@
 class CreateHauntedHouses < ActiveRecord::Migration[4.2]
 
-  def change 
+  def change
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location
@@ -10,8 +10,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
       t.datetime :opening_date
       t.datetime :closing_date
       t.text :description
-      
 
 
 
-  end 
+
+  end
