bb3a9acfb514b31fb864f16bb55acc0daac028ff
Mitchel Parish <mitchelparish@gmail.com>
2019-02-19 18:45:00 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,4 +1,4 @@
-class CreateCostumes < ActiveRecord::Migration 
+class CreateCostumes < ActiveRecord::Migration
 
   def change
     create_table :costumes do |t|
@@ -8,7 +8,7 @@ class CreateCostumes < ActiveRecord::Migration
       t.string :size
       t.timestamps
     end      
-  end 
+  end
 
 
 # Create a class and inherit from ActiveRecord::Migration
@@ -18,5 +18,5 @@ class CreateCostumes < ActiveRecord::Migration
 # 002_create_costumes.rb becomes class CreateCostumes
 
 # Define a change method in which to do the migration
-# In this change method, create columns with the correct names and 
+# In this change method, create columns with the correct names and
 # value types according to the spec
